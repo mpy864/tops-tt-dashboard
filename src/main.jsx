07158fddd94ad5face8501cc8daed517bb8bc2d7
“MPY” <mohitprakashyadav.tops.sai@gmail.com>
2026-04-02 04:23:16 +0530

    fix: correct App import casing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import app from './app'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
