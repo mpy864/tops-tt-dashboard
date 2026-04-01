cccb7040abd900b35546cbc29f47975021d4c13a
“MPY” <mohitprakashyadav.tops.sai@gmail.com>
2026-04-02 04:20:03 +0530

    feat: H2H comparison dashboard at /h2h with stats, matrix, rank chart, W/L breakdown

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import app from './app'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
